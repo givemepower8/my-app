13e00aff24d4b5d4b75a265e8592aab210eab7a5
givemepower8 <givemepower8@gmail.com>
2022-08-30 11:30:36 -0700

    minor

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Welcome from './doodle/Welcome'
 
 function App() {
+  
   return (
+    
     <div className="App">
       <header className="App-header">
+        <Welcome name="Wei" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
---
+++ src/doodle/Welcome.tsx
@@ -0,0 +1,7 @@
+interface WelcomeProps {
+    name: string; 
+}
+
+export default function Welcome(props:WelcomeProps) {
+    return <h1>Hello, {props.name}</h1>;
+}
